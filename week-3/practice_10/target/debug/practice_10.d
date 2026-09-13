ab2841ed7e00ecf9583f3b6a29c0c84031a03a71
Lisa <lisa.ikhalo@pau.edu.ng>
2026-09-14 00:05:44 +0100

    week 3 cargo, data type and mutability

---
+++ week-3/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\lisai\OneDrive\Documents\l.ikhaloCOS101\week-3\practice_10\target\debug\practice_10.exe: C:\Users\lisai\OneDrive\Documents\l.ikhaloCOS101\week-3\practice_10\src\main.rs
---
+++ week-3/practice_7/target/debug/practice_7.d
@@ -0,0 +1,1 @@
+C:\Users\lisai\OneDrive\Documents\l.ikhaloCOS101\week-3\practice_7\target\debug\practice_7.exe: C:\Users\lisai\OneDrive\Documents\l.ikhaloCOS101\week-3\practice_7\src\main.rs
